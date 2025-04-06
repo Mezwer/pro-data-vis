0f747bed8f7c7158f325f55b6825a399b50d89a1
Mezwer <junkemailgoggles@gmail.com>
2025-04-06 02:17:12 -0500

    route excluded by gitignore

---
+++ pro-data-vis/src/app/api/data/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { collectGraphData } from "@/app/actions.js";
+
+export async function GET(req) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const playername = searchParams.get("playername");
+    
+    const start = performance.now()
+    const data = await collectGraphData(playername);
+    const end = performance.now()
+
+    console.log(`TIME OF GRAPH COLLECTION: ${end - start} MS`);
+    return NextResponse.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
