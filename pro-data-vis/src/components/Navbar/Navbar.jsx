38f66ca187ce0b308380b0b0289a5bf5eee3fad2
Mezwer <junkemailgoggles@gmail.com>
2025-08-11 18:27:45 -0500

    forgot to change navbar

--- pro-data-vis/src/components/Navbar/Navbar.jsx
+++ pro-data-vis/src/components/Navbar/Navbar.jsx
@@ -6,8 +6,12 @@ const Navbar = () => {
   return (
     <div className="w-screen bg-transparent h-[4rem] flex flex-row justify-between px-10 items-center">
       <div className="flex flex-row gap-4">
-        <Link className="cursor-pointer" href="/"> Home </Link>
-        <Link className="cursor-pointer" href="/info"> About </Link>
+        <Link className="cursor-pointer" href="/">
+          Home
+        </Link>
+        <Link className="cursor-pointer" href="/about">
+          About
+        </Link>
       </div>
 
       <NavbarSearch></NavbarSearch>
